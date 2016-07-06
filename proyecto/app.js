6f8381a7cbd5862cb4cb3ace6d2507a05e846065
HernaldoSanchez <hsanchez@fss.local>
2016-07-06 15:51:33 -0400

    Video ultimo video

--- proyecto/app.js
+++ proyecto/app.js
@@ -1,21 +1,31 @@
 var express = require("express");
 var bodyParser = require("body-parser");
 var User = require("./models/user").User;
-var session = require("express-session");
+//var session = require("express-session");
+var cookieSession = require("cookie-session");
 var router_app = require("./routes_app");
 var session_middleware = require("./middlewares/session");
-
+var methodOverride = require("method-override");
 
 var app = express();
 
-app.use("/estatico",express.static('public'));
+app.use("/public",express.static('public'));
 app.use(bodyParser.json()); //peticiones application/json
 app.use(bodyParser.urlencoded({extended: true})); //peticiones application/json
+app.use(methodOverride("_method"));
+
 app.use(express.static('assets'));
+
+/* Con session
 app.use(session({
 	secret: "sdfasdfasdf1223",
 	resave: false,
 	saveUninitialized: false
+}));
+*/
+app.use(cookieSession({
+	name: "session",
+	keys: ["llave-1","llave-2"]
 }));
 
 app.set("view engine", "jade");
@@ -59,9 +69,8 @@ app.post("/users", function(req,res){
 
 app.post("/sessions", function(req,res){
 	User.findOne({email:req.body.email,password:req.body.password},function(err,user){
-		console.log(user);
 		req.session.user_id= user._id;
-		res.send("Hola Mundo");
+		res.redirect("/app");
 	});
 });
 
--- proyecto/routes_app.js
+++ proyecto/routes_app.js
@@ -1,10 +1,63 @@
 var express = require("express");
-
+var Imagen = require("./models/imagenes");
 var router = express.Router();
 
 router.get("/",function(req,res){
 	/*Busacar el usuario*/
 	res.render("app/home");
 });
+
+/* REST */
+
+router.get("/imagenes/new",function(req,res){
+	res.render("app/imagenes/new");
+});
+router.get("/imagenes/:id/edit",function(req,res){
+	Imagen.findById(req.params.id,function(err,imagen){
+		res.render("app/imagenes/edit",{imagen:imagen});
+	});
+	
+});
+
+router.route("/imagenes/:id")
+	.get(function(req,res){
+		Imagen.findById(req.params.id,function(err,imagen){
+			res.render("app/imagenes/show",{imagen:imagen});
+		});
+		
+	})
+	.put(function(req,res){
+		Imagen.findById(req.params.id,function(err,imagen){
+			imagen.title= req.body.title;
+		
+
+		});
+
+	})
+	.delete(function(req,res){
+		
+	});
+
+router.route("/imagenes")
+	.get(function(req,res){ //obtener la collection de imagines
+		Imagen.find({},function(err,imagenes){
+			res.render("app/imagenes/index",{imagenes: imagenes});
+		});
+	})
+	.post(function(req,res){ //guardar la colecion de imagenes
+		var data = {
+			title: req.body.title
+		}
+		var imagen = new Imagen(data);
+		
+		imagen.save(function(err){
+			if(!err){
+				res.redirect("/app/imagenes/"+imagen._id);
+			}
+			else{
+				res.render(err);
+			}
+		});
+	});
 
 module.exports = router;
